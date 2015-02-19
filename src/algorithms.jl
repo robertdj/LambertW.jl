1c6e9f3af04b2def29216c55080bdd10a7e9aa7d
Robert DJ <git@dahl-jacobsen.dk>
2015-02-19 14:56:08 +0100

    Multiple dispatch using value types
    The initial approximations are split into separate functions for each
    branch using value types.

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -15,7 +15,7 @@ function lambertw(x::Real, k::Int=0, prec=eps())
 	end
 
 	# First approximation
-	W = lambertwApprox(x, k)
+	W = lambertwApprox(x, Val{k})
 
 	# Compute residual using logarithms to avoid numerical overflow
 	# When x == 0, r = NaN, but here the approximation is exact and 
@@ -44,25 +44,29 @@ function lambertw{T<:Real}(x::Array{T}, k::Int=0, prec=eps())
 end
 
 
-function lambertwApprox(x::Real, k::Int)
-	if k == -1
-		const M1 = 0.3361
-		const M2 = -0.0042
-		const M3 = -0.0201
+# Initial approximation for branch 0
+function lambertwApprox(x::Real, ::Type{Val{0}})
+	if x <= 1
+		eta = 2 + 2*exp(1)*x;
+		N2 = 3*sqrt(2) + 6 - (((2237+1457*sqrt(2))*exp(1) - 4108*sqrt(2) - 5764)*sqrt(eta))/((215+199*sqrt(2))*exp(1) - 430*sqrt(2)-796);
+		N1 = (1-1/sqrt(2))*(N2+sqrt(2));
 
-		sigma = -1 - log(-x)
-        W = -1 - sigma - 2/M1*(1 - 1/(1 + (M1*sqrt(sigma/2)) / (1 + M2*sigma*exp(M3*sqrt(sigma)))))
-	elseif k == 0
-		if x <= 1
-            eta = 2 + 2*exp(1)*x;
-            N2 = 3*sqrt(2) + 6 - (((2237+1457*sqrt(2))*exp(1) - 4108*sqrt(2) - 5764)*sqrt(eta))/((215+199*sqrt(2))*exp(1) - 430*sqrt(2)-796);
-            N1 = (1-1/sqrt(2))*(N2+sqrt(2));
-
-            W = -1 + sqrt(eta)/(1 + N1*sqrt(eta)/(N2 + sqrt(eta)));
-		else
-            W = log( 6*x/(5*log( 12/5*(x/log(1+12*x/5)) )) )
-		end
+		W = -1 + sqrt(eta)/(1 + N1*sqrt(eta)/(N2 + sqrt(eta)));
+	else
+		W = log( 6*x/(5*log( 12/5*(x/log(1+12*x/5)) )) )
 	end
+
+	return W
+end
+
+# Initial approximation for branch -1
+function lambertwApprox(x::Real, ::Type{Val{-1}})
+	const M1 = 0.3361
+	const M2 = -0.0042
+	const M3 = -0.0201
+
+	sigma = -1 - log(-x)
+	W = -1 - sigma - 2/M1*(1 - 1/(1 + (M1*sqrt(sigma/2)) / (1 + M2*sigma*exp(M3*sqrt(sigma)))))
 
 	return W
 end
